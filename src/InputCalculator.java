48aa1eaaf62cab4e73a2040c865b9a0676cdc1b0
ayokunle adeleye <adeways2000@gmail.com>
2021-06-07 13:42:14 +0200

    Initial commit

---
+++ src/InputCalculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class InputCalculator {
+
+    public static void inputThenPrintSumAndAverage(){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        int counter = 0;
+
+        while(true){
+            boolean input = scanner.hasNextInt();
+            if(input) {
+                int inputInt = scanner.nextInt();
+                sum += inputInt;
+                counter++;
+            }
+            else{
+                double average = (double) sum / counter;
+                System.out.println("SUM = " + sum + " AVG = " + Math.round(average));
+                break;
+            }
+            scanner.nextLine();
+        }
+
+        scanner.close();
+    }
+}
